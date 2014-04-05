68413cf714fb5418ee7a616e64b9f3192e655751
Prathap <notprathap@gmail.com>
2014-04-05 14:56:10 +0100

    API end point reachable

--- src/API/Bundle/RestBundle/Controller/DefaultController.php
+++ src/API/Bundle/RestBundle/Controller/DefaultController.php
@@ -2,12 +2,33 @@
 
 namespace API\Bundle\RestBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
-class DefaultController extends Controller
+class DefaultController extends FOSRestController
 {
     public function indexAction($name)
     {
         return $this->render('APIRestBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * This is the documentation description of your method, it will appear
+     * on a specific pane. It will read all the text until the first
+     * annotation.
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Get all dataset descriptions",
+     *  filters={
+     *      {"name"="search-term", "dataType"="string"}
+     *  }
+     * )
+     */
+    public function getItemsAction()
+    {
+        $view = $this->view(['item1', 'item2'], 200);
+
+        return $this->handleView($view);
     }
 }
