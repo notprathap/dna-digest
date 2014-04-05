27baeefd14485a32cdd8f35a022f3e8e8eafa5b3
Prathap <notprathap@gmail.com>
2014-04-05 16:29:39 +0100

    excluding items on issue

--- src/API/Bundle/RestBundle/Entity/Issue.php
+++ src/API/Bundle/RestBundle/Entity/Issue.php
@@ -3,10 +3,14 @@
 namespace API\Bundle\RestBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Exclude;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="issue")
+ *
+ * @ExclusionPolicy("none")
  */
 class Issue
 {
@@ -23,6 +27,7 @@ class Issue
     protected $description;
 
     /**
+     * @Exclude
      * @ORM\ManyToOne(targetEntity="Item", inversedBy="issues")
      * @ORM\JoinColumn(name="item_id", referencedColumnName="id")
      */
